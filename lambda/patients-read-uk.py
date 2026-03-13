218e265eb1ee7ca69198a5d46eab5b04f1567e7d
Sam <sam@Sams-MacBook-Pro.local>
2026-03-13 19:52:34 +0000

    Fix path parameter name in patients-read Lambda

--- lambda/patients-read-uk.py
+++ lambda/patients-read-uk.py
@@ -5,7 +5,7 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('patients-uk')
 
 def lambda_handler(event, context):
-    patient_id = event['pathParameters']['id']
+    patient_id = event['pathParameters']['patient_id']
     response = table.get_item(Key={
         "PK": f"PATIENT#{patient_id}",
         "SK": "METADATA"
