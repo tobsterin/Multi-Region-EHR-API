1c3fee768b708c2b6226df12c2d3fb40c7c57126
Sam <243201945+tobsterin@users.noreply.github.com>
2026-06-23 11:43:02 +0100

    Add overwrite protection and generic nationalId field to write path

--- terraform/modules/patient-api/src/write_patient.py
+++ terraform/modules/patient-api/src/write_patient.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
@@ -18,19 +19,32 @@ def lambda_handler(event, context):
             "patient_id": event['id'],
             "resourceType": "Patient",
             "name": f'{event["name"][0]["family"]}, {" ".join(event["name"][0]["given"])}',
-            "nhsNumber": event["identifier"][0]["value"],
+            "nationalId": event["identifier"][0]["value"],
             "gender": event["gender"],
             "birthDate": event["birthDate"],
             "address": f'{", ".join(event["address"][0]["line"])}, {event["address"][0]["city"]}, {event["address"][0]["postalCode"]}, {event["address"][0]["country"]}',
             "generalPractitioner": event["generalPractitioner"][0]["display"]
         }
+        # To prevent accidental overwrite
+        try:
+            response = table.put_item(
+                Item=item,
+                ConditionExpression="attribute_not_exists(patient_id)"
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                return {
+                    "statusCode": 409,
+                    "body": json.dumps({"error": "Patient already exists"})
+                }
+            else:
+                raise
     else:
         return {
             "statusCode": 400,
             "body": json.dumps({"error": f"Unsupported resourceType: {resource_type}"})
         }
 
-    response = table.put_item(Item=item)
     print("DynamoDB response:", json.dumps(response))
 
     return {
