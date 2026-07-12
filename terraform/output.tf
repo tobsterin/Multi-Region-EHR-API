3ee49d11ba31812c5909591d0b40751a6614c2f7
Sam <243201945+tobsterin@users.noreply.github.com>
2026-07-12 21:12:48 +0100

    Cognito output added to output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,14 @@
+#cognito
+output "cognito_user_pool_id" {
+  description = "The ID of the Cognito User Pool."
+  value       = module.cognito.cognito_user_pool_id
+}
+output "cognito_user_pool_client_id" {
+  description = "The ID of the Cognito User Pool Client."
+  value       = module.cognito.cognito_user_pool_client_id
+}
+
+
 #mpi_url:
 output "table_arn_global" {
   description = "The ARN of the global DynamoDB table."
